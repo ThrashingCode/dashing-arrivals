a228dc10b518e58a5b424a510fc908e07b8095cc
Adron Hall <adronhall@gmail.com>
2015-04-04 19:22:26 -0700

    Added test file.

---
+++ test/test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by adronhall on 4/4/15.
+ */
+
+var assert = require("assert")
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    })
+  })
+})
